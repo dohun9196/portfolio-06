e04708c98328bc9d827f7530d083fa5973ac3016
Dohun9196 <68097610+dohun9196@users.noreply.github.com>
2023-01-02 23:48:51 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,149 @@
+
+
+$(function () {
+
+    $('.mainVisual .mainSlide').slick({
+        dots: false,
+        autoplay: false,
+        arrows: true,
+
+        prevArrow: $('#prev'),
+        nextArrow: $('#next'),
+        autoplaySpeed: 4000,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+    });
+
+
+    $('.down_con a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+    $('.kawasaki_group ul>li a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+    //이벤트 상시 대기
+    $(window).load(function () {
+        console.log("onload : start");
+    });
+
+    $(window).scroll(function () {
+        var scrollValue = $(document).scrollTop();
+        console.log(scrollValue);
+    });
+
+    // $(window).scroll(function () {
+    //     var height = $(document).scrollTop();
+
+    //     if (height > 2100) {
+    //         $('.slideTitle').addClass('fix');
+    //     } else if (height < 2100) {
+    //         $('.slideTitle').removeClass('fix');
+    //     }
+    // });
+
+    // $(window).on('resize', function () {
+    //     if ($(window).width() > 769) {
+    //         $(window).scroll(function () {
+    //             var mheight = $(document).scrollTop();
+
+    //             if (mheight > 1461) {
+    //                 $('.slideTitle').addClass('fix');
+    //             } else if (mheight < 1461) {
+    //                 $('.slideTitle').removeClass('fix');
+    //             }
+    //         })
+    //     }
+    // });
+
+
+
+
+    $(window).resize(function () {
+        if (window.innerWidth > 769) {
+            // 다바이스 크기가 769이상일때 
+            $(window).scroll(function () {
+                var height = $(document).scrollTop();
+
+                if (height > 2100) {
+                    $('.slideTitle').addClass('fix');
+                } else if (height < 2100) {
+                    $('.slideTitle').removeClass('fix');
+                }
+            })
+
+        } else {
+            // 다바이스 크기가 769이하일때 
+            $(window).scroll(function () {
+                var mheight = $(document).scrollTop();
+
+                if (mheight > 1461) {
+                    $('.slideTitle').addClass('fix');
+                } else if (mheight < 1461) {
+                    $('.slideTitle').removeClass('fix');
+                }
+            })
+
+        }
+    }).resize();
+
+
+
+
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true,
+        'alwaysShowNavOnTouchDevices': true,
+        // 'disableScrolling': true,  
+        // { 이게 있으면 over, close 되었을 때 x축으로 스크롤바 생김 }
+        'alwaysShowClose': true,
+        // 'maxWidth': 1200,
+        // 'maxHeight': 800,
+        'fitImagesInViewport': true,
+
+    });
+
+    $('.top_btn a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+        return false;
+    });
+    $("#top_btn").scrollTop(0);
+
+
+    $('.mbtn').on('click', function () {
+        $(this).toggleClass('on')
+        $('.gnb').toggleClass('on')
+    });
+
+    $('.gnb>ul>li>a').on('click', function (e) {
+        if ($(window).width() < 769) {
+            e.preventDefault();
+            $(this).parent().siblings().find('.dropdown').hide();
+            $(this).next().toggle();
+        }
+    });
+
+    $(window).on('resize', function () {
+        if ($(window).width() > 768) {
+            $('.dropdown').removeAttr('style');
+        }
+    });
+
+
+
+});
+
+
+
+
