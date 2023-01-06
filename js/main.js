f3b68df31e05b7d163755316457ada73fe393db8
Dohun9196 <68097610+dohun9196@users.noreply.github.com>
2023-01-06 15:53:48 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -54,19 +54,16 @@ $(function () {
     $('.wedding_nav .inner.content .gall_nav .gall_num').on('click', function () {
         $('.wedding_nav .inner.content .gall_nav .gall_num').not($(this)).removeClass('on');
         $(this).toggleClass('on')
+    });
+    $('.right .right_Topbox .r_other .other_flex span').on('click', function () {
+        $('.right .right_Topbox .r_other .other_flex span').not($(this)).removeClass('on');
+        $(this).toggleClass('on')
 
     });
 
 
 
     $('.down_con a').click(function () {
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 400);
-        return false;
-    });
-
-    $('.kawasaki_group ul>li a').click(function () {
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 400);
@@ -83,76 +80,7 @@ $(function () {
         console.log(scrollValue);
     });
 
-    // $(window).scroll(function () {
-    //     var height = $(document).scrollTop();
 
-    //     if (height > 2100) {
-    //         $('.slideTitle').addClass('fix');
-    //     } else if (height < 2100) {
-    //         $('.slideTitle').removeClass('fix');
-    //     }
-    // });
-
-    // $(window).on('resize', function () {
-    //     if ($(window).width() > 769) {
-    //         $(window).scroll(function () {
-    //             var mheight = $(document).scrollTop();
-
-    //             if (mheight > 1461) {
-    //                 $('.slideTitle').addClass('fix');
-    //             } else if (mheight < 1461) {
-    //                 $('.slideTitle').removeClass('fix');
-    //             }
-    //         })
-    //     }
-    // });
-
-
-
-
-    $(window).resize(function () {
-        if (window.innerWidth > 769) {
-            // 다바이스 크기가 769이상일때 
-            $(window).scroll(function () {
-                var height = $(document).scrollTop();
-
-                if (height > 2100) {
-                    $('.slideTitle').addClass('fix');
-                } else if (height < 2100) {
-                    $('.slideTitle').removeClass('fix');
-                }
-            })
-
-        } else {
-            // 다바이스 크기가 769이하일때 
-            $(window).scroll(function () {
-                var mheight = $(document).scrollTop();
-
-                if (mheight > 1461) {
-                    $('.slideTitle').addClass('fix');
-                } else if (mheight < 1461) {
-                    $('.slideTitle').removeClass('fix');
-                }
-            })
-
-        }
-    }).resize();
-
-
-
-
-    lightbox.option({
-        'resizeDuration': 200,
-        'wrapAround': true,
-        'alwaysShowNavOnTouchDevices': true,
-        // 'disableScrolling': true,  
-        // { 이게 있으면 over, close 되었을 때 x축으로 스크롤바 생김 }
-        'alwaysShowClose': true,
-        // 'maxWidth': 1200,
-        // 'maxHeight': 800,
-        'fitImagesInViewport': true,
-
-    });
 
     $('.top_btn a').click(function () {
         $('html, body').animate({
@@ -165,24 +93,4 @@ $(function () {
 
 
 
-    $('.gnb>ul>li>a').on('click', function (e) {
-        if ($(window).width() < 769) {
-            e.preventDefault();
-            $(this).parent().siblings().find('.dropdown').hide();
-            $(this).next().toggle();
-        }
-    });
-
-    $(window).on('resize', function () {
-        if ($(window).width() > 768) {
-            $('.dropdown').removeAttr('style');
-        }
-    });
-
-
-
 });
-
-
-
-
